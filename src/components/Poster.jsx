7561331d6ec058c92131dd76116b0a04630e1056
Andres Puello <andresfpuello16@gmail.com>
2023-12-07 17:11:10 -0400

    commitseven

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import "./Poster.css";
-import Fade from 'react-reveal/Fade';
 
 const Poster = () => {
     return (
         <div className='Poster' id="home">
-            <Fade top>
-                <h1>Do you need a Plumber?</h1>
-            </Fade>
+            <h1>Do you need a Plumber?</h1>
             <br />
             <p>Your reliable solution for all your plumbing problems!</p>
         </div>
